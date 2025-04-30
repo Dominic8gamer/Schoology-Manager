7439f162663d971576d1d303f1db3beac291bd83
Liamr-dev <lreine27@cgps.org>
2025-04-30 12:48:52 -0400

    Added simple drag and drop (needs rework) + name of tasks

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let value = ""
+let childCounter = 1;
 
 function getValue() {
   let inputField = document.getElementById("taskName");
   value = inputField.value;
 }
 
+function handleDragStart(e) {
+  e.dataTransfer.setData('text/plain', e.target.id);
+}
+
 function addTask() {
     let newTask = document.createElement("p");
     newTask.innerHTML = value;
+    newTask.draggable = true;
+    newTask.id = "task-" + new Date().getTime();
+    newTask.addEventListener('dragstart', handleDragStart);
     document.getElementById("incompleteTasks").appendChild(newTask);
     document.getElementById('taskName').value = ""
   }
@@ -16,5 +24,3 @@ function addTask() {
     document.getElementById("addTaskButton").addEventListener("click", getValue);
     document.getElementById("addTaskButton").addEventListener("click", addTask);
   };
-
-
